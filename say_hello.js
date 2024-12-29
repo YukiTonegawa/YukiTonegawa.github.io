f219a2c150a2f54b412662f45ba0a8d102f7891c
tonegawa <tonegawa@tonegawanoMacBook-Air.local>
2024-12-30 05:26:23 +0900

    test commit

--- say_hello.js
+++ say_hello.js
@@ -1,2 +1,2 @@
-document.write("こんにちは");
-document.write("<script src=ファイル名></script>でもJavaScriptのコードを実行できます。");
+document.write("こんにちは\n");
+document.write("\<script src=ファイル名\>\</script\>でもJavaScriptのコードを実行できます。");
