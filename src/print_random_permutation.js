486f24b67700aa6e1f35471a5d0a067aa216940c
tonegawa <tonegawa@tonegawanoMacBook-Air.local>
2024-12-30 19:10:58 +0900

    test commit

--- src/print_random_permutation.js
+++ src/print_random_permutation.js
@@ -1,8 +1,9 @@
-import {random_permutation} from "./random.js";
-
+import {random_permutation} from "./src/random.js";
+document.open();
+document.write("この文章はファイルから表示されています。" + "<br>");
 document.write("長さ10のランダムな順列を表示します。" + "<br>");
-
 random_permutation(10).forEach(function(elem) {
     document.write(elem, " ");
 });
 document.write("<br>");
+document.close();
--- src/random.js
+++ src/random.js
@@ -1,29 +1,28 @@
-/*
 // jsの数値型はdouble
 // [0, R)
-function random_int(R) {
+export function random_int(R) {
     return Math.floor(Math.random() * R);
 }
 
 // [0, R)
-function random_float(R) {
+export function random_float(R) {
     return Math.random() * R;
 }
 
 // [L, R)
-function random_int_range(L, R) {
+export function random_int_range(L, R) {
     let len = R - L;
     return random_int(len) + L;
 }
 
 // [L, R)
-function random_float_range(L, R) {
+export function random_float_range(L, R) {
     let len = R - L;
     return random_float(len) + L;
 }
 
 // [L, R) * N
-function random_int_array(N, L, R) {
+export function random_int_array(N, L, R) {
     let res = [];
     for (let i = 0; i < N; i++) {
         res.push(random_int_range(L, R));
@@ -32,14 +31,13 @@ function random_int_array(N, L, R) {
 }
 
 // [L, R) * N
-function random_float_array(N, L, R) {
+export function random_float_array(N, L, R) {
     let res = [];
     for (let i = 0; i < N; i++) {
         res.push(random_float_range(L, R));
     }
     return res;
 }
-*/
 
 export function random_shuffle(V) {
     let N = V.length, tmp, i;
