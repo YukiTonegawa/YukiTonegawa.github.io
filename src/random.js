7883fee077045e8435d23d1cdc44c9e0bde77f2d
tonegawa <tonegawa@tonegawanoMacBook-Air.local>
2024-12-31 03:56:43 +0900

    test commit

--- src/random.js
+++ src/random.js
@@ -1,28 +1,28 @@
 // jsの数値型はdouble
 // [0, R)
-export function random_int(R) {
+function random_int(R) {
     return Math.floor(Math.random() * R);
 }
 
 // [0, R)
-export function random_float(R) {
+function random_float(R) {
     return Math.random() * R;
 }
 
 // [L, R)
-export function random_int_range(L, R) {
+function random_int_range(L, R) {
     let len = R - L;
     return random_int(len) + L;
 }
 
 // [L, R)
-export function random_float_range(L, R) {
+function random_float_range(L, R) {
     let len = R - L;
     return random_float(len) + L;
 }
 
 // [L, R) * N
-export function random_int_array(N, L, R) {
+function random_int_array(N, L, R) {
     let res = [];
     for (let i = 0; i < N; i++) {
         res.push(random_int_range(L, R));
@@ -31,7 +31,7 @@ export function random_int_array(N, L, R) {
 }
 
 // [L, R) * N
-export function random_float_array(N, L, R) {
+function random_float_array(N, L, R) {
     let res = [];
     for (let i = 0; i < N; i++) {
         res.push(random_float_range(L, R));
@@ -39,7 +39,7 @@ export function random_float_array(N, L, R) {
     return res;
 }
 
-export function random_shuffle(V) {
+function random_shuffle(V) {
     let N = V.length, tmp, i;
     while (N) {
         i = Math.floor(Math.random() * N--);
@@ -51,12 +51,10 @@ export function random_shuffle(V) {
 }
 
 // [1, N]
-export function random_permutation(N) {
+function random_permutation(N) {
     let res = [];
     for (let i = 0; i < N; i++) {
         res.push(i + 1);
     }
     return random_shuffle(res);
 }
-
-
