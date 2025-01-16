6d53abebe491d4a06b88f57c7dfbd8df57896c26
tonegawa <tonegawa@tonegawanoMacBook-Air.local>
2025-01-16 17:17:42 +0900

    index segtree

--- src/graph_d3_template.js
+++ src/graph_d3_template.js
@@ -14,31 +14,48 @@ function histogram_template(V) {
     const marginRight = 20;
     const marginBottom = 30;
     const marginLeft = 80;
+    
     const x = d3.scaleLinear()
         .domain([0, V.length + 1])
         .range([marginLeft, width - marginRight]);
+    
     const y = d3.scaleLinear()
-        .domain([0, calc_max(V) * 2])
+        .domain([Math.min(0, calc_min(V) * 1.5), Math.max(Math.abs(calc_min(V)), calc_max(V)) * 1.5])
         .range([height - marginBottom, marginTop]);
+    
     const svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto;");
+    
     svg.append("g")
         .attr("fill", "steelblue")
         .selectAll()
         .data(data)
         .join("rect")
         .attr("x", d => x(d.id) - (x(d.id + 1) - x(d.id)) / 2)
-        .attr("width", d => x(d.id + 1) - x(d.id))
-        .attr("y", d => y(d.value))
-        .attr("height", d => height - marginBottom - y(d.value));
+        .attr("width", d => x(d.id + 1) - x(d.id) - 1)
+        .attr("y", function(d) {
+            if (d.value >= 0) {
+                return y(d.value);
+            } else {
+                return y(0);
+            }
+        })
+        .attr("height", function(d) {
+            if (d.value >= 0) {
+                return y(0) - y(d.value);
+            } else {
+                return y(d.value) - y(0);
+            }
+        });
     
     let idx = [];
     for (let i = 1; i <= V.length; i++) idx.push(i);
+
     svg.append("g")
-        .attr("transform", `translate(0, ${height - marginBottom})`)
+        .attr("transform", `translate(0, ${y(0)})`)
         .call(d3.axisBottom(x).tickValues(idx).tickFormat(d3.format("d")))
         .call(g => g.append("text")
         .attr("x", width)
@@ -79,13 +96,14 @@ function cartesian_tree_template(V) {
             dep[v] = (p == -1 ? 0 : dep[p] + 1);
         }
     }
+    let depmax = Math.max(...dep);
     let data = {"nodes": [], "links": []};
     let data_node = [];
     let data_link = [];
     for (let i = 0; i < N; i++) {
         let d = {
-            "x": 100 + i * 70,
-            "y": 100 + dep[i] * 70,
+            "x": i,
+            "y": dep[i],
             "id": i + 1,
             "value": V[i],
         }
@@ -104,13 +122,17 @@ function cartesian_tree_template(V) {
     data["links"] = data_link;
     const width = 1000;
     const height = 600;
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
     const links = data.links.map(d => ({...d}));
     const nodes = data.nodes.map(d => ({...d}));
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).strength(0))
-        .on("tick", ticked);
+    
+    const x = d3.scaleLinear()
+        .domain([-1, V.length])
+        .range([0, width]);
 
+    const y = d3.scaleLinear()
+        .domain([-1, depmax + 1])
+        .range([0, height]);
+    
     // Create the SVG container.
     const svg = d3.create("svg")
         .attr("width", width)
@@ -124,78 +146,45 @@ function cartesian_tree_template(V) {
         .selectAll()
         .data(links)
         .join("line")
-        .attr("stroke-width", d => 3)
-
+        .attr("x1", d => x(nodes[d.source - 1]["x"]))
+        .attr("y1", d => y(nodes[d.source - 1]["y"]))
+        .attr("x2", d => x(nodes[d.target - 1]["x"]))
+        .attr("y2", d => y(nodes[d.target - 1]["y"]))
+        .attr("stroke-width", d => 2)
+    
     const node = svg.append("g")
         .attr("stroke", "black")
         .attr("stroke-width", 1.5)
         .selectAll()
         .data(nodes)
         .join("ellipse")
-        .attr("rx", 40)
-        .attr("ry", 30)
+        .attr("cx", d => x(d.x))
+        .attr("cy", d => y(d.y))
+        .attr("rx", Math.min(40, width / (V.length + 1)))
+        .attr("ry", Math.min(30, height / (V.length + 1)))
         .attr("fill", "white");
 
+    let font_size = Math.max(6, Math.min(40, width / (V.length + 1)) / 2);
     const label = svg.append("g")
         .attr("stroke", "#ff9")
-        .attr("stroke-width", 0.1)
+        .attr("stroke-width", 0.0)
         .selectAll()
         .data(nodes)
         .join("text")
         .attr("class", "center")
-        .attr("font-size", "15px")
+        .attr("font-size", font_size + "px")
+        .attr("x", d => x(d.x))
+        .attr("y", d => y(d.y + 0.05))
         .attr("fill", "black") 
         .attr("style", "text-anchor:middle;")
         .text(d => d.value);
 
     const border_width = 1;
-    const borderL = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", border_width)
-        .attr("height", height)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
+    const borderL = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderR = svg.append("rect").attr("x", width - border_width).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderD = svg.append("rect").attr("x", 0).attr("y", height - border_width).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderU = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
 
-    const borderR = svg.append("rect")
-        .attr("x", width - border_width)
-        .attr("y", 0)
-        .attr("width", border_width)
-        .attr("height", height)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    const borderD = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", height - border_width)
-        .attr("width", width)
-        .attr("height", border_width)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    const borderU = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", width)
-        .attr("height", border_width)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    function ticked() {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-
-        label
-            .attr("x", d => d.x)
-            .attr("y", d => d.y + 5);
-    }
     return svg.node();
 }
 
@@ -382,37 +371,10 @@ function general_graph_template(N, g, E, is_directed, is_weighted, H = -1, W = -
         .text(d => d.id);
     
     const border_width = 1;
-    const borderL = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", border_width)
-        .attr("height", height)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    const borderR = svg.append("rect")
-        .attr("x", width - border_width)
-        .attr("y", 0)
-        .attr("width", border_width)
-        .attr("height", height)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    const borderD = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", height - border_width)
-        .attr("width", width)
-        .attr("height", border_width)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
-
-    const borderU = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", width)
-        .attr("height", border_width)
-        .attr("stroke", "grey")
-        .attr("stroke-width", border_width)
+    const borderL = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderR = svg.append("rect").attr("x", width - border_width).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderD = svg.append("rect").attr("x", 0).attr("y", height - border_width).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderU = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
     
     node.call(d3.drag()
         .on("start", dragstarted)
@@ -515,12 +477,6 @@ function geo_2d_point_template(P, E, is_weighted = false, H = -1, W = -1) {
     let data = {"nodes": [], "links": []};
     let data_node = [];
     let data_link = [];
-    let maxidx = 0;
-    for (let i = 0; i < P.length; i++) {
-        let tmp = Math.max(Math.abs(P[i][0]), Math.abs(P[i][1]));
-        if (maxidx < tmp) maxidx = tmp;
-    }
-    if (maxidx == 0) maxidx = 1;
 
     for (let i = 0; i < N; i++) {
         let d = {
@@ -552,8 +508,35 @@ function geo_2d_point_template(P, E, is_weighted = false, H = -1, W = -1) {
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto;");
 
-    let x = d3.scaleLinear().domain([-maxidx * width / height, maxidx * width / height]).range([0, width]);
-    let y = d3.scaleLinear().domain([maxidx, -maxidx]).range([0, height]); // 上がy軸正
+    let xmax = -Infinity, xmin = Infinity, ymax = -Infinity, ymin = Infinity;
+    for (let i = 0; i < P.length; i++) {
+        xmax = Math.max(xmax, P[i][0]);
+        xmin = Math.min(xmin, P[i][0]);
+        ymax = Math.max(ymax, P[i][1]);
+        ymin = Math.min(ymin, P[i][1]);
+    }
+
+    if (P.length == 0) {
+        xmax = 1, xmin = 0;
+        ymax = 1, ymin = 0;
+    }
+    if ((xmax - xmin) > (ymax - ymin) * width / height) {
+        let rem = (xmax - xmin) / width * height - (ymax - ymin);
+        ymin -= rem / 2;
+        ymax += rem / 2;
+    } else {
+        let rem = (ymax - ymin) * width / height - (xmax - xmin);
+        xmin -= rem / 2;
+        xmax += rem / 2;
+    }
+    let dx = xmax - xmin, dy = ymax - ymin;
+    xmin -= dx * 0.3;
+    xmax += dx * 0.3;
+    ymin -= dy * 0.3;
+    ymax += dy * 0.3;
+
+    let x = d3.scaleLinear().domain([xmin, xmax]).range([0, width]);
+    let y = d3.scaleLinear().domain([ymax, ymin]).range([0, height]); // 上がy軸正
 
     let axis_x = d3.axisBottom(x)
         .ticks(15 * width / height)
@@ -643,6 +626,76 @@ function geo_2d_point_template(P, E, is_weighted = false, H = -1, W = -1) {
         .attr("style", "text-anchor:middle;")
         .text(d => d.id);
 
+    return svg.node();
+}
+
+function segtree_generator(_N) {
+    // _N <= 128に制限する
+    const width = 1300
+    const height = 600;
+    const marginTop = 30;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 20;
+    let log = 0;
+    while ((1 << log) < _N) log++;
+    let size = 1 << log;
+    let data = {"nodes": [], "links": []};
+    let data_node = [];
+    let data_link = [];
+    for (let i = 1; i < 2 * size; i++) {
+        let d = {
+            "id": i,
+            "hbit": 31 - Math.clz32(i)
+        };
+        data_node.push(d);
+    }
+
+    for (let i = 2; i < 2 * size; i++) {
+        let d = {
+            "source": Math.floor(i / 2),
+            "target": i,
+        }
+        data_link.push(d);
+    }
+    data["nodes"] = data_node;
+    data["links"] = data_link;
+
+    const height_row = (height - marginBottom - marginTop) / (log + 1);
+    const x = d3.scaleLinear().domain([0, size]).range([marginLeft, width - marginRight]);
+    const y = d3.scaleLinear().domain([log, 0]).range([height - marginBottom - height_row, marginTop]);
+    const links = data.links.map(d => ({...d}));
+    const nodes = data.nodes.map(d => ({...d}));
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+
+    const border_width = 1;
+    const borderL = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderR = svg.append("rect").attr("x", width - border_width).attr("y", 0).attr("width", border_width).attr("height", height).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderD = svg.append("rect").attr("x", 0).attr("y", height - border_width).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
+    const borderU = svg.append("rect").attr("x", 0).attr("y", 0).attr("width", width).attr("height", border_width).attr("stroke", "grey").attr("stroke-width", border_width);
+    
+    svg.append("g")
+        .attr("fill", "none")
+        .selectAll()
+        .data(nodes)
+        .join("rect")
+        .attr("x", function(d) {
+            let lmost = 1 << d.hbit;
+            return x((d.id - lmost) << (log - d.hbit));
+        })
+        .attr("width", function(d) {
+            let lmost = 1 << d.hbit;
+            return x((d.id + 1 - lmost) << (log - d.hbit)) - x((d.id - lmost) << (log - d.hbit));
+        })
+        .attr("y", d => y(d.hbit))
+        .attr("height", function(d) { return height_row;})
+        .attr("stroke", "black")
+        .attr("stroke-width", "2px");
     return svg.node();
 }
 
